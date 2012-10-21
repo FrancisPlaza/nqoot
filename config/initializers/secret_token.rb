554bdd0e433694430a3cc07825a375453230a715
Francis Plaza <efcplaza@gmail.com>
2012-10-21 19:25:12 -0400

    created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EfcplazaProj3::Application.config.secret_token = 'f5a9682b9b35c316fb45f38bf1de418b602ef4d5f644479e2276924e9fea3014b2814166fe4ac38e2fee378c0c5ec449ef5576e25ee7c4e4ab7e9f170825a6df'
