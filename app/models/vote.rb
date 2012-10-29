a1970919181f6b701446d9eee6a0051acff73186
Francis Plaza <efcplaza@gmail.com>
2012-10-28 19:46:39 -0700

    fix syntax error on vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,9 +1,10 @@
 class Vote < ActiveRecord::Base
   attr_accessible :answer_id, :user_id
   
-  def self.add_vote(params, uid):
+  def self.add_vote(params, uid)
     vote = Vote.new
     vote.answer_id = params[:answer_id]
     vote.user_id = current_user.id
     vote.save!
+  end
 end
