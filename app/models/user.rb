4e4bc980e313f16ab5321428c0478c07bcb0cf47
Francis Plaza <efcplaza@gmail.com>
2012-11-04 21:17:54 -0500

    added is staff option in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.is_staff = false
       user.save!
     end
   end
