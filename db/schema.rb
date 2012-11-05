1be417a65cd9cfa57b8a0e8d828da5b8333b3419
Francis Plaza <efcplaza@gmail.com>
2012-11-04 22:15:19 -0500

    updated answer field to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121105023122) do
+ActiveRecord::Schema.define(:version => 20121105031457) do
 
   create_table "answers", :force => true do |t|
-    t.string   "answer"
     t.boolean  "anonimity"
     t.integer  "question_id"
     t.datetime "created_at",        :null => false
@@ -22,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20121105023122) do
     t.string   "user_id"
     t.integer  "vote"
     t.boolean  "is_staff_endorsed"
+    t.text     "answer"
   end
 
   create_table "questions", :force => true do |t|
